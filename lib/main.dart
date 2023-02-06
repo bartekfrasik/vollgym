e9b0706b3f7cf4f7f64072561320681f0ee51ca7
Bartek Frasik <bartole1995@gmail.com>
2023-02-06 12:02:03 +0100

    organize files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:vollgym/app/home/home_page.dart';
+import 'package:vollgym/app/login/login_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -20,7 +23,28 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: const RootPage(),
     );
+  }
+}
+
+class RootPage extends StatelessWidget {
+  const RootPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          final user = snapshot.data;
+          if (user == null) {
+            return const LoginPage();
+          }
+          {
+            return HomePage(user: user);
+          }
+        });
   }
 }
